e07e4bf3a70ecef13262ee3e3113e474b8e2126f
a60231023 <ayushtiwari6061@gmail.com>
2022-09-28 22:19:54 +0530

    next project uploading no. 6

---
+++ 07QRCodeGenerator - API/App.js
@@ -0,0 +1,26 @@
+const mainDiv = document.getElementById("main");
+const input = document.getElementById("input");
+const qrGeneratorBtn = document.getElementById("submit");
+const qrCode = document.getElementById("img");
+qrGeneratorBtn.addEventListener("click", () => {
+  if (!input.value.length == 0) {
+    qrGeneratorBtn.innerHTML = "Generating QR Code...";
+    qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+  }
+  qrCode.addEventListener("load", () => {
+    qrGeneratorBtn.innerHTML = "Generate QR Code";
+    qrCode.hidden = false;
+    const downloadImg = document.createElement("a");
+    downloadImg.style.display = "block";
+    downloadImg.setAttribute(
+      "download",
+      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`
+    );
+    downloadImg.setAttribute(
+      "href",
+      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`
+    );
+    downloadImg.innerHTML = "Download QR Code";
+    mainDiv.appendChild(downloadImg);
+  });
+});
