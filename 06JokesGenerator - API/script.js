9e4850da808686a23707ff5c0cb3633fec9bb5e9
a60231023 <ayushtiwari6061@gmail.com>
2022-10-24 23:55:23 +0530

    safe mode enabled

--- 06JokesGenerator - API/script.js
+++ 06JokesGenerator - API/script.js
@@ -1,15 +1,36 @@
 const jokeinput = document.getElementById("joke");
 
 const apicall = async () => {
-  const val = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
+  const val = await fetch("https://v2.jokeapi.dev/joke/Any?type=single/value=safe-mode");//enabling safe mode gives many jokes undefined
   console.log(val);
   const first = await val.json();
   console.log(first);
-  const joke = first.joke;
+  const joke = await first.joke;
   console.log(joke);
-  jokeinput.innerText = joke;
+  if(joke == undefined){
+    alert('not for kids');
+  }else{
+    jokeinput.innerText = joke;
+  
+  }
   console.log("button clicked");
 };
 const btn = document.getElementById("jokeBtn");
 console.log(btn);
 btn.addEventListener("click", apicall);
+
+// fetch("https://v2.jokeapi.dev/joke/Any?type=single/value=safe-mode")
+//   .then((response) => {
+//     // const first = response.json();
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((second) => {
+//     console.log(second);
+//   });
+
+// fetch("https://v2.jokeapi.dev/joke/Any?type=single/value=safe-mode")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// // console.log(apicall2);
