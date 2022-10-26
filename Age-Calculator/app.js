db5b5a9c3941309daf3be7abf496a79a5e57e7de
a60231023 <ayushtiwari6061@gmail.com>
2022-10-26 23:44:35 +0530

    modify and next project

--- Age-Calculator/app.js
+++ Age-Calculator/app.js
@@ -1,17 +1,68 @@
-const container;
-const error;
+// const container;
+const error = document.getElementsByClassName("error");
+console.log(error);
+const dob = document.getElementById("dob");
+console.log(dob);
+let value;
+// dob.addEventListener("input", () => {
+//     value = dob.value;
+//     console.log(value);
+// })
 
-function getAge(){
-    
-    
+const yearss = document.getElementById("years");
+const monthss = document.getElementById("months");
+const dayss = document.getElementById("days");
+function getAge() {
+  dob.addEventListener("input", () => {
+    value = dob.value;
+    console.log(value);
+    calcAge(value);
+  });
 }
 
-function calcAge(){ 
-    
+function calcAge(value) {
+  let current = new Date(value);
+  console.log(current);
+  let diff = new Date() - current;
+  if (diff < 0) {
+    error[0].style.display = "block";
+  }
+
+  console.log(diff);
+  let diff_in_sec = diff / 1000;
+  let format = new Date(diff);
+  console.log(format);
+  let year = format.getUTCFullYear() - 1970;
+  console.log(year);
+
+  const month = Math.floor(diff_in_sec / 3600 / (24 * 30)) % 12;
+  console.log(month);
+
+  const days = Math.floor(diff_in_sec / 3600 / 24) % 30;
+  console.log(days);
+
+  // converting to hours
+
+  const hours = Math.floor((diff_in_sec / 3600) % 24);
+  console.log(hours);
+
+  // convertin to minture
+
+  const minutes = Math.floor((diff_in_sec / 60) % 60);
+  console.log(minutes);
+
+  const seconds = Math.floor(diff_in_sec % 60);
+  console.log(seconds);
+
+  const result = { year, month, days, hours, minutes, seconds };
+  console.log(result);
+  displayAge(result);
 }
 
-function displayAge(){
-    
-    
+function displayAge(result) {
+  yearss.innerText = result.year;
+  monthss.innerText = result.month;
+  dayss.innerText = result.days;
 }
 
+getAge();
